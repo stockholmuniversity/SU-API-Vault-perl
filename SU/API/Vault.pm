0740d8139ca4f2c1623d44b12ecbc460038032a7
Mikael Nordin <mikael.nordin@su.se>
2018-07-25 14:09:17 +0200

    perltidy

--- SU/API/Vault.pm
+++ SU/API/Vault.pm
@@ -65,7 +65,7 @@ sub login
 
 sub logout
 {
-    my ($self)  = @_;
+    my ($self) = @_;
     my $request_url = "$self->{url}/v1/auth/token/revoke-self";
     my $req = HTTP::Request->new(POST => $request_url);
     $req->header('X-Vault-Token' => $self->{client_token});
@@ -76,11 +76,11 @@ sub logout
     }
     else
     {
-        $self->{login_status}  = "not logged in";
-        $self->{logged_in}     = 0;
-        $self->{exp_time}      = time;
-        $self->{client_token}  = undef;
-        $self->{last_secret}   = undef;
+        $self->{login_status} = "not logged in";
+        $self->{logged_in}    = 0;
+        $self->{exp_time}     = time;
+        $self->{client_token} = undef;
+        $self->{last_secret}  = undef;
     }
     return $self->{login_status};
 
